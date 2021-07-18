972e3a5f5ff4550ad2d05a28edccbda363d8c434
This2sho <zx00018@naver.com>
2021-07-19 04:11:06 +0900

    edit multiplication 문제 수정

---
+++ boj/Mutiplication.java
@@ -0,0 +1,24 @@
+package boj;
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        int first = scanner.nextInt();
+        int second = scanner.nextInt();
+        
+        int second_1 = second % 10;
+        int second_2 = second % 100 / 10;
+        int second_3 = second / 100;
+        
+        int one = first * second_1;
+        int two = first * second_2;
+        int three = first * second_3;
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+        System.out.print(one+two*10+three*100);
+
+    }
+}

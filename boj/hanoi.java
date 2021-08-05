a93f8f25f3fbbe41f39a4da164b92a9264962e82
This2sho <zx00018@naver.com>
2021-08-06 01:46:10 +0900

    edit hanoi String -> StringBuffer (String은 새로운값 할당할 때마다 새로 생성.. StringBuffer는 값은 Memory에 append하는 방식)

--- boj/hanoi.java
+++ boj/hanoi.java
@@ -1,7 +1,12 @@
 package boj;
 
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
+
 
 /*
 boj # 11729
@@ -13,89 +18,52 @@ boj # 11729
 첫째 줄에 옮긴 횟수 K를 출력한다.
 두 번째 줄부터 수행 과정을 출력한다. 두 번째 줄부터 K개의 줄에 걸쳐 두 정수 A B를 빈칸을 사이에 두고 출력하는데, 이는 A번째 탑의 가장 위에 있는 원판을 B번째 탑의 가장 위로 옮긴다는 뜻이다.
 
-1개일 때 
-<1> -> <3>으로
+처음 문제 풀 때 StringBuffer를 사용하지않고 String으로 더해서 값을 구했더니
+String(tmp)에 값을 더해줄때마다 새로 생성함으로 각 String의 주소값이 stack에 쌓이고 class들은 garbage collector가 호출되기 전까지
+heap에 지속적으로 쌓이게 되는걸 몰랐다.. 메모리 측면에서도 치명적이고 속도 또한 시간제한에 걸렸다.
 
-2개일 때 1개를 2로 옮기고 1
-<1> -> <2>
+String대신 StringBuffer 클래스를 사용함으로서 tmp의 값이 변화해도 같은 메모리에 append하는 방식이라 클래스가 생성됨으로서 method들과 variable들이 생성되는 시간이 추가적으로 들지않아
+시간제한에 안걸린것 같다.
++1) StringBuffer와 StringBuilder의 차이는 둘 다 변경가능한 문자열이지만 StringBuilder는 synchronization이 적용되지 않고,
+    StringBuffer는 적용되어 multiple thread에서 안전한 클래스라고 한다.
 
-<1> -> <3>
++2) 자바 알고리즘에서 입/출력에서 시간 단축하는 방법
 
-<2> -> <3>
+    입력 : Scanner 대신 BufferedReader를 사용한다.
+    예시 : BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-3개일 때 (위에 2개를 2로 옮기는 작업 후 무거운거 3으로 )
-<1> -> <3>
-<1> -> <2>
-<3> -> <2>
-
-<1> -> <3>
-
-<2> -> <1>
-<2> -> <3>
-<1> -> <3>
-
-4개일 때 (위에 3개를 2로 옮기는것 과 같은 작업후 무거운거 3으로 옮기고 2에 있는걸 3으로 옮기는 작업과 같음)
-<1> -> <2>
-<1> -> <3>
-<2>
+    출력 : System.out.print 대신 BufferedWriter를 사용한다.
+    예시 : static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 */
 public class hanoi {
-    static String tmp = "";
-    static int cnt = 0;
+    static StringBuffer tmp = new StringBuffer();
+    static int sum = 0;
 
-    static class res_hanoi{
-        private String move = "";
-        private int sum = 0;
+    // public static StringBuilder sb = new StringBuilder();
+    
 
-        public res_hanoi(String move, int sum){
-            this.move = move;
-            this.sum  = sum;
-        }
-    }
-
-    static HashMap<Integer, res_hanoi> cache = new HashMap<Integer, res_hanoi>();
-
-
-    public static int else_num(int a, int b){
-        switch(a+b){
-            case 3 :
-                return 3;
-            case 4 :
-                return 2;
-            case 5 :
-                return 1;
-            default :
-                return -1;            
-        }
-    }
-    public static void do_hanoi(int n, int src, int des){
-        int key = n*100 + src*10 + des;
-        // key 있을 때
-        if(cache.containsKey(key)){
-            cnt += cache.get(key).sum;
-            tmp += cache.get(key).move;
-        }else{
-            if(n == 1){
-                cnt++;
-                tmp += src + " " + des + "\n";
-            }
-            else {
-                do_hanoi(n-1, src, else_num(src, des));
-                do_hanoi(1, src, des);
-                do_hanoi(n-1, else_num(src, des), des);
-            }
-        }
-    }
-
-    public static void main(String[] args){
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
         // while(N <= 0){
         //     System.out.println("0이상 넣어라.");
         //     N = scanner.nextInt();
         // }
-        do_hanoi(N, 1, 3);
-        System.out.println(cnt);
-        System.out.print(tmp);
+        do_hanoi(N, 1, 2, 3);
+        bw.write((int) sum + "\n");
+        bw.append(tmp);
+    }
+
+    public static void do_hanoi(int n, int src, int mid, int des){
+        if(n == 1){
+            sum++;
+            tmp.append(src + " " + des + "\n");
+        }
+        else {
+            do_hanoi(n-1, src, des, mid);
+            do_hanoi(1, src, mid, des);
+            do_hanoi(n-1, mid, src, des);
+        }
     }
 }

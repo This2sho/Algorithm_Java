301542a994bcbda81731e57cd49b43e434a231db
This2sho <zx00018@naver.com>
2022-02-25 19:07:45 +0900

    Add: BOJ Queue card2

---
+++ boj/card2.java
@@ -0,0 +1,19 @@
+class card2{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        LinkedList<Integer> q = new LinkedList<>();
+        for(int i=1; i<=N; i++){
+            q.add(i);
+        }
+
+        while(q.size() == 1){
+            q.poll();
+            int top = q.poll();
+            q.add(top);
+        }
+
+        System.out.println(q.poll());
+    }
+}

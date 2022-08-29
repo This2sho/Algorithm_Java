6d90beb2c83c1be44d80c55e3836653217252ea4
This2sho <zx00018@naver.com>
2022-08-29 17:25:41 +0900

    Ps : 백준 25305 풀이

---
+++ boj/N25305.java
@@ -0,0 +1,23 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+// 커트라인
+public class N25305 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        Integer[] integers = Arrays.stream(arr).boxed().sorted(Comparator.reverseOrder()).toArray(Integer[]::new);
+        System.out.print(integers[k-1]);
+    }
+}

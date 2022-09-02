4fd8642a4e1475b98bbe92fa3ec4a6075056fcc2
This2sho <zx00018@naver.com>
2022-09-02 23:30:37 +0900

    Ps : 프로그래머스 자물쇠와 열쇠 문제

---
+++ boj/week6/hidden/자물쇠와열쇠.java
@@ -0,0 +1,12 @@
+package boj.week6.hidden;
+
+public class 자물쇠와열쇠 {
+    public static void main(String[] args) {
+        자물쇠와열쇠 자물쇠와열쇠 = new 자물쇠와열쇠();
+
+    }
+    public boolean solution(int[][] key, int[][] lock) {
+        boolean answer = true;
+        return answer;
+    }
+}

018d64645971e377f96743ea016bf55c3e70d8d8
This2sho <zx00018@naver.com>
2022-09-06 00:14:40 +0900

    Ps: 백준 1009번 문제 풀이 다시

--- boj/N1009.java
+++ boj/N1009.java
@@ -7,14 +7,19 @@ import java.util.StringTokenizer;
 
 public class N1009 {
     public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+        int N = Integer.parseInt(br.readLine());
 
-    while (N-- > 0) {
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-    }
-
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int result = a % 10;
+            for (int i = 1; i < b; i++) result = (result * a) % 10;
+            if(result == 0) result = 10;
+            sb.append(result + "\n");
+        }
+        System.out.print(sb);
     }
 }
